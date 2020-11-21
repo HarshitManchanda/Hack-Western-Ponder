f070326dd0bfb4e3041a02885f360c4ea6945d1b
Harshit Manchanda <hmanchanda27@gmail.com>
2020-11-22 00:59:46 +0530

    Modified the front page

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.top_section}>
+      <Text style={{fontSize: 18}}>Hello,</Text>
+      <Text style={{fontSize: 18}}>Welcome to Ponder</Text>
+      </View>
       <StatusBar style="auto" />
+      <View style={styles.middle_section}>
+        <Image source={require('./assets/logo.png')} style={{ width: 200, height: 70, resizeMode: 'contain' }} />
+        <Text style={{fontSize: 18}}>Your new personal self-care companion</Text>
+      </View>
+      <View style={styles.bottom_section}>
+        <Text style={{fontSize: 18}}>Start your journey</Text>
+        <Text style={{fontSize: 18}}>to explore your inner self!</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +26,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#5A57EF',
+    // alignItems: 'flex-start',
+    // justifyContent: 'center',
   },
+  top_section: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    marginTop: 120,
+    marginLeft: 30,
+    // marginBottom: -100
+  },
+  middle_section: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    // marginTop: 100,
+  },
+  bottom_section: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    marginRight: 30,
+    marginTop: 100,
+  }
 });
