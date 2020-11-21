07ca8fd4ac1c94c40dd2ca021bd79eb766b56458
Harshit Manchanda <hmanchanda27@gmail.com>
2020-11-22 02:54:11 +0530

    Finished front page

--- App.js
+++ App.js
@@ -1,24 +1,33 @@
 import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert, TouchableHighlight, ImageBackground} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <ImageBackground source={require('./assets/background.png')} style={styles.image}>
       <View style={styles.top_section}>
-      <Text style={{fontSize: 18}}>Hello,</Text>
-      <Text style={{fontSize: 18}}>Welcome to Ponder</Text>
+        <Text style={{fontSize: 18}}>Hello,</Text>
+        <Text style={{fontSize: 18}}>Welcome to Ponder</Text>
       </View>
       <StatusBar style="auto" />
       <View style={styles.middle_section}>
-        <Image source={require('./assets/logo.png')} style={{ width: 200, height: 70, resizeMode: 'contain' }} />
+        <Image source={require('./assets/logo.png')} style={{ width: 250, height: 80, resizeMode: 'contain' }} />
         <Text style={{fontSize: 18}}>Your new personal self-care companion</Text>
+        <TouchableHighlight underlayColor="#FFF8F8" onPress={() => Alert.alert('Button Pressed')}>
+          <View style={styles.button}>
+            <Text style={{fontSize: 18}}>Get Started</Text>
+            </View>
+        </TouchableHighlight>
       </View>
       <View style={styles.bottom_section}>
         <Text style={{fontSize: 18}}>Start your journey</Text>
         <Text style={{fontSize: 18}}>to explore your inner self!</Text>
       </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -28,28 +37,39 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#5A57EF',
-    // alignItems: 'flex-start',
-    // justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+  button: {
+    backgroundColor: "#FC545E",
+    padding: 15,
+    margin: 20,
+    width: 150,
+    alignItems: 'center',
+    borderRadius: 10
   },
   top_section: {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'flex-start',
-    marginTop: 120,
+    marginTop: 100,
     marginLeft: 30,
-    // marginBottom: -100
+    marginBottom: 50
   },
   middle_section: {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    // marginTop: 100,
   },
   bottom_section: {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'flex-end',
+    justifyContent: 'flex-end',
     marginRight: 30,
-    marginTop: 100,
+    marginBottom: 100,
   }
 });
