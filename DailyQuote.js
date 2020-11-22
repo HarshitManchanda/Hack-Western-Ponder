861c3e33bf683ff40a319dfcfe5d2a9d50648c94
Harshit Manchanda <hmanchanda27@gmail.com>
2020-11-22 07:59:04 +0530

    Added Daily Quote page

---
+++ DailyQuote.js
@@ -0,0 +1,54 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image, Button, Alert, TouchableHighlight, ImageBackground} from 'react-native';
+
+export default function DailyQuote() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.section}>
+          <Image source={require('./assets/sun.png')} style={styles.image}/>
+          <Text style={styles.header_text}>Morning, Sunshine!</Text>
+        </View>
+        <View style={styles.section}>
+          <Text style={{fontSize: 18}}>Today we are going to have an awesome day</Text>
+          <Text style={{fontSize: 18}}>filled with positive experiences!</Text>
+        </View>
+        <View style={styles.section}>
+          <Text style={{fontSize: 18}}>Throughout the day, keep in mind how</Text>
+          <Text style={{fontSize: 18}}>you could make today 5% better than yesterday</Text>
+        </View>
+        <View style={styles.section}>
+          <Text style={{fontSize: 24, fontWeight: "600"}}>"With the new day comes new</Text>
+          <Text style={{fontSize: 24, fontWeight: "600"}}>strength and new thoughts."</Text>
+          <Text style={{fontSize: 20}}>- Eleanor Roosevelt</Text>
+        </View>
+        <View style={styles.section}>
+          <Text style={{fontSize: 20}}>I AM READY FOR AN AWESOME DAY!!!</Text>
+        </View>
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: '#9AE0F6',
+    },
+    section: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    image: {
+      width: 150, 
+      height: 150, 
+      marginTop: 120, 
+      marginBottom: 10
+    },
+    header_text: {
+      fontSize: 34, 
+      marginBottom: 20, 
+      fontWeight: "500"
+    }
+  });
